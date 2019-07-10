7bae0e7c9e6c5b7af84288493c7d66d41519a8a4
mfekadu <mike.fekadu@gmail.com>
2019-07-10 18:41:03 -0400

    made a simple doc

---
+++ make_doc.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cargo doc --open --document-private-items
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::collections::VecDeque;
+
+
+/// tokenizer
+/// given an S-expression as a string, output a stream of tokens
+fn tokenizer(s : String) -> VecDeque<String> {
+    let mut v = VecDeque::new();
+    v.push_back(s);
+    v
+}
+
 fn main() {
     println!("Hello, world!");
+    // todo: tokenizer
+    // given (+ 2 2) >> ["(", "+", "2", "2", ")"]
+    // use VecDeque
+    tokenizer("applesauce".to_string());
 }
