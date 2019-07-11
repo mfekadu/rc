6ba47f5a15254a42464c120bd9b21ff91d710086
mfekadu <mike.fekadu@gmail.com>
2019-07-11 11:39:14 -0400

    ideas
    Co-Authored-By: johnvtan <johnvtan@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,61 @@
 use std::collections::VecDeque;
 
-enum SomeEnum { Haha }
+enum Keyword {
+    Let,
+    Program
+}
 
-/// tokenizer
+enum Operation {
+    Plus,
+    Negation,
+    Read
+}
+
+/// an expression type for the R1 language
+/// exp ::= int | (read) | (- exp) | (+ exp exp) | var | (let ([var exp]) exp)
+/// R1 ::= (program info exp)
+enum R1Expr {
+    Num { v: u64 }, // TODO: check book for what range int has
+    // Read, // e.g. (read)
+    // Negation { exp: Box<R1Expr> }, // e.g. (- 2)
+    // Addition { exp1: Box<R1Expr>, exp2: Box<R1Expr> }, // e.g. (+ 2 2)
+    NullaryOperation { op: Operation }, // e.g. (read)
+    UnaryOperation { op: Operation, e1: Box<R1Expr> }, // e.g. (- 2)
+    BinaryOperation { op: Operation, e1: Box<R1Expr>, e2: Box<R1Expr> }, // e.g. (+ 2 2)
+    // Function { op: Operation, exp: Vec<Box<R1Expr>> }
+    Binding, // e.g. (let ([x 10]) (+ x 2))
+    // TODO: is this even what a var should be like?
+    Var { symbol: String } // e.g. x
+    // Var { v: &'static str },
+}
+
+/// abstract syntax tree
+
+
 /// given an S-expression as a string, output a stream of tokens
+/// e.g. (program () (+ 2 2))
 fn tokenizer(s : String) -> VecDeque<String> {
     let mut v = VecDeque::new();
     v.push_back(s);
     v
 }
 
-/// parser
-/// given tokens, output an abstract syntax tree
-fn parser(_s : VecDeque<String>) -> SomeEnum {
-    SomeEnum::Haha
+/// given tokens, output an abstract syntax tree of R1
+fn parse_r1(_tokens : VecDeque<String>) -> R1Expr {
+    R1Expr::Num{value: 42}
 }
 
-/// uniqify
 /// given an abstract syntax tree, output an AST with unique variable names
-fn uniqify(_s : SomeEnum) -> SomeEnum {
-    SomeEnum::Haha
+fn uniqify_r1(_ : R1Expr) -> R1Expr {
+    R1Expr::Num{value: 42}
 }
 
-/// main
 /// a compiler for the R1 langauge
 fn main() {
     println!("Hello, RC!");
 
-    uniqify(parser(tokenizer("applesauce".to_string())));
+    match uniqify_r1(parse_r1(tokenizer("applesauce".to_string()))) {
+        R1Expr::Num{v} => { println!("matched number with value = {}", v) }
+        _ => { println!("idk") }
+    }
 }
