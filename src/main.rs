058aef6669fee2932e43a41a5e683c850689d693
mfekadu <mike.fekadu@gmail.com>
2019-07-11 12:47:28 -0400

    tokenizer is tokenizing sorta.. it has null strings and stuff
    Co-Authored-By: johnvtan <johnvtan@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
 use std::collections::VecDeque;
-
-enum Keyword {
-    Let,
-    Program
-}
+use std::collections::HashMap;
 
 enum Operation {
     Plus,
@@ -15,47 +11,77 @@ enum Operation {
 /// exp ::= int | (read) | (- exp) | (+ exp exp) | var | (let ([var exp]) exp)
 /// R1 ::= (program info exp)
 enum R1Expr {
-    Num { v: u64 }, // TODO: check book for what range int has
-    // Read, // e.g. (read)
-    // Negation { exp: Box<R1Expr> }, // e.g. (- 2)
-    // Addition { exp1: Box<R1Expr>, exp2: Box<R1Expr> }, // e.g. (+ 2 2)
-    NullaryOperation { op: Operation }, // e.g. (read)
-    UnaryOperation { op: Operation, e1: Box<R1Expr> }, // e.g. (- 2)
+    Num (u64), // TODO: check book for what range int has
+    NullaryOperation( Operation ), // e.g. (read)
+    UnaryOperation{ op: Operation, e: Box<R1Expr> }, // e.g. (- 2)
     BinaryOperation { op: Operation, e1: Box<R1Expr>, e2: Box<R1Expr> }, // e.g. (+ 2 2)
-    // Function { op: Operation, exp: Vec<Box<R1Expr>> }
-    Binding, // e.g. (let ([x 10]) (+ x 2))
-    // TODO: is this even what a var should be like?
-    Var { symbol: String } // e.g. x
-    // Var { v: &'static str },
+    // Function { op: Operation, exp: Vec<R1Expr> } // TODO: for R2
+    Binding { // e.g. (let ([x 10]) (+ x 2))
+        var: Box<R1Expr>,
+        value: Box<R1Expr>,
+        body: Box<R1Expr>
+    },
+    Var( String ) // e.g. x
 }
 
-/// abstract syntax tree
+/// the program
+struct R1 {
+    info: HashMap<String, String>,
+    exp: R1Expr
+}
 
 
 /// given an S-expression as a string, output a stream of tokens
 /// e.g. (program () (+ 2 2))
 fn tokenizer(s : String) -> VecDeque<String> {
-    let mut v = VecDeque::new();
-    v.push_back(s);
-    v
+
+    let mut tokens = VecDeque::new();
+
+    let mut token = String::new();
+
+    let specials = "(){}[]"; // TODO: consider comments via ';'
+
+    for c in s.chars() {
+        if specials.contains(c) {
+            // we found a special
+            // lets look for words
+            if (token.len() > 0) {
+                tokens.push_back(token.clone()); // TODO: avoid clone
+                token.clear();
+            }
+            tokens.push_back(c.to_string());
+        } else if " ".contains(c) {
+            tokens.push_back(token.clone()); // TODO: avoid clone
+            token.clear();
+        } else {
+            token.push(c);
+        }
+    }
+
+    tokens
 }
 
 /// given tokens, output an abstract syntax tree of R1
 fn parse_r1(_tokens : VecDeque<String>) -> R1Expr {
-    R1Expr::Num{value: 42}
+    R1Expr::Num(42)
 }
 
 /// given an abstract syntax tree, output an AST with unique variable names
 fn uniqify_r1(_ : R1Expr) -> R1Expr {
-    R1Expr::Num{value: 42}
+    R1Expr::Num(42)
 }
 
 /// a compiler for the R1 langauge
 fn main() {
     println!("Hello, RC!");
 
-    match uniqify_r1(parse_r1(tokenizer("applesauce".to_string()))) {
-        R1Expr::Num{v} => { println!("matched number with value = {}", v) }
+    let input = "(program () (let ([x 2])(+ x (let {{x 3}} x))".to_string();
+
+    println!("{:?}", tokenizer(input.clone()));
+
+    match uniqify_r1(parse_r1(tokenizer(input.clone()))) {
+        R1Expr::Num(v) => { println!("matched number with value = {}", v) }
         _ => { println!("idk") }
     }
 }
+
