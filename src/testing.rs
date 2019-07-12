2d5919a856b950e59d15bb97016f57d83d39ceea
mfekadu <mike.fekadu@gmail.com>
2019-07-12 12:08:21 -0400

    Update testing.rs

--- src/testing.rs
+++ src/testing.rs
@@ -4,7 +4,7 @@ mod tokenizer_tests {
     #[test]
     fn test_add() {
         let input = String::from("(+ 2 2)");
-        assert_eq!(tokenizer(input), vec!["(", "+", "2", "2", ")"]); 
+        assert_eq!(tokenizer(input), vec!["(", "+", "2", "2", ")"]);
     }
 
     #[test]
@@ -12,17 +12,19 @@ mod tokenizer_tests {
         let input = String::from("(let ([x 2]) (+ x 4))");
         let output = tokenizer(input);
         let expect = vec!["(", "let", "(", "[", "x", "2", "]", ")", "(", "+", "x", "4", ")", ")"];
-        assert_eq!(output, expect); 
+        assert_eq!(output, expect);
     }
 
     #[test]
     fn test_full_program() {
         let given = "(program () (let ([x 2])(+ x (let {{x 3}} x))".to_string();
-        let expect = crate::tokenizer(given.clone());
-        let output = crate::tokenizer(given.clone());
-        assert_eq!(expect, output);
+        let expect = vec!["(", "program", "(", ")", "(", "let", "(", "[", "x", "2", "]", ")", "(",
+        "+", "x", "(", "let", "{", "{", "x", "3","}", "}", "x", ")", ")"];
+        let output = tokenizer(given);
+        // fun fact: dont do assert_eq!(expect, output);
+        // because &str does not impl PartialEq but String does
+        assert_eq!(output, expect);
     }
-
 }
 
 
