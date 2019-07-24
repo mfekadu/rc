19885b2eab2cf9ccba013b925837f1630e4002ac
mfekadu <mike.fekadu@gmail.com>
2019-07-24 06:39:31 -0400

    added 3 passing tests
    test complex in the val of binding
    
    test complex in the body of binding
    
    test both together

--- racket/rco.rkt
+++ racket/rco.rkt
@@ -195,6 +195,15 @@
 (displayln (list "test..." (check-true (is_complex_and_has_sym? some_let_expr))))
 
 
+(define complex_val_let_expr '(rco_arg '(let ((x (- 2))) 2)))
+(displayln (list "test..." (check-true (is_complex_and_has_sym? some_let_expr))))
+
+(define complex_body_let_expr '(rco_arg '(let ((x 2)) (- 2))))
+(displayln (list "test..." (check-true (is_complex_and_has_sym? some_let_expr))))
+
+(define complex_BOTH_let_expr '(rco_arg '(rco_arg '(let ((x (- 2))) (- 2)))))
+(displayln (list "test..." (check-true (is_complex_and_has_sym? some_let_expr))))
+
 ;TEST ERROR for malformed let expression
 (define bad_let_expr '(let (x 2) 2))
 (check-fail (λ () (rco_exp bad_let_expr)))
