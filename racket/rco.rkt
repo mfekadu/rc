a20713bb79b52588917ffc8cc40fd4393d02eb0f
mfekadu <mike.fekadu@gmail.com>
2019-07-23 23:13:23 -0400

    very cool tests and thought process in messy comments
    hash is fun with rkt
    
    tests with helper funcs are great because helper funcs can be tested themselves... todo get rid of that coverage highlight on the #f in the _ arm of the test match

--- racket/rco.rkt
+++ racket/rco.rkt
@@ -25,10 +25,11 @@
      ]
     ; match operation and rest of args
     [(list op args ...)
+     
 
      (check-true (<= (length args) 2))
 
-     (println (list "args:" args))
+     (print (list "input:" e)) (println (list "args:" args))
      (define simplify (λ (e) (rco_arg e)))
 
      (println (cons "map!" (map simplify args)))
@@ -57,7 +58,7 @@
 
      (define alist (hash tmp_name (rco_exp l)))
 
-     (list tmp_name alist)
+     (cons tmp_name alist)
      ]
     [_ "panic!"]))
 
@@ -84,11 +85,46 @@
 (check-equal? (rco_exp '(let (x 2) x)) '(let (x 2) x))
 (check-equal? (rco_exp (list 'read)) '(read))
 ;; SIMPLE exprs SHOULD STAY SIMPLE
-;(check-true (match (rco_arg (list '+ 2 2))
-;                  [(make-immutable-hash (list (cons 'tmp31467 (list '+ 2 2)))) ]))
+;;;; ??? isn't any list passed into rco_arg complex?
+;;;; ... hmmm.... i guess so? idk...
+(check-true (match (rco_arg '(+ 2 2))
+              [(cons new_sym alist)
+               (and (symbol? new_sym)
+                    (hash? alist)
+                    (hash-has-key? alist new_sym))]
+              ; the _ arm should never happen. ignore coverage highlight
+              [_ #f]))
+
+;TODO: this test case way get reconsidered? if a let is complex?
+; because rco_arg is the simplify function
 ;(check-equal? (rco_arg '(let (x 2) x)) '(let (x 2) x))
+
+; so is a READ complex?
+; well.. yes if it is inside of something like (+ 2 (read))
+; certainly x86 assembly would prefer to have (let ([tmp (read)]) (+ 2 tmp))
+; well then by that same logic... a let expr should also be considered complex? right?
+; hmmm.... good point.
 ;(check-equal? (rco_arg (list 'read)) '(read))
 
 
 
+; HELPER FOR rco_arg tests
+; given a complex arg
+; return true if proper output (pair of symbol and hash where hash has symbol)
+; else false
+(define (proper_rco_arg_output_on_complex_arg arg)
+  (match (rco_arg arg)
+    [(cons new_sym alist)
+     (and (symbol? new_sym)
+          (hash? alist)
+          (hash-has-key? alist new_sym))]
+    ; the _ arm should never happen. ignore coverage highlight
+    [_ #f]))
+
+
+  
+
+
+; HASH STUFF
 ;(hash-eq #hash((1 . 1)) (make-immutable-hash (list (cons 1 1))))
+;(and (hash? (make-immutable-hash)) (hash-has-key? (make-hash (list (cons 's 2))) 's))
