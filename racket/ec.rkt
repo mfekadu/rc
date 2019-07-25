3b7627ba0c6b17d0a4cbbf6af3a478923b0b4703
mfekadu <mike.fekadu@gmail.com>
2019-07-25 15:27:06 -0400

    ec following instructor
    https://github.com/jnear/compiler-construction-assignments/blob/master/compiler-A2.rkt

--- racket/ec.rkt
+++ racket/ec.rkt
@@ -7,29 +7,50 @@
 
 (define (ec_tail e)
   (match e
-    [(list 'let bind body)
-
-     ; TODO: get the fancy cond in rco.rkt in the same let match arm of rco_exp
-     ; TODO: functionalize this fancy cond
-     (cond [(and (list? bind)
-                 (list? (first bind)))]
-           [else (error "expect let_expr in form (let ([var expr]) expr)")])
-
-     ; we can match first of bind because of the above conditional
-     (define output (ec_assign bind (ec_tail body)))
+    [(? symbol?) `(return ,e)]
+    [(? integer?) `(return ,e)]
+    [`(let ([,var ,val]) ,body) ; fancy destructuring syntax
+     (define output (ec_assign val var (ec_tail body)))
      (displayln output)
      output]
-    [_ `(return ,e)]))
+    [`(,op ,args ...) `(return ,e)]))
+;    [(list 'let bind body)
+;
+;     ; TODO: get the fancy cond in rco.rkt in the same let match arm of rco_exp
+;     ; TODO: functionalize this fancy cond
+;     (cond [(and (list? bind)
+;                 (list? (first bind)))]
+;           [else (error "expect let_expr in form (let ([var expr]) expr)")])
+;
+;     ; we can match first of bind because of the above conditional
+;     (define output (ec_assign bind (ec_tail body)))
+;     (displayln output)
+;     output]
+;    [_ `(return ,e)]))
 
-(define (ec_assign e tail)
-  (match e
-    [(list bind)
-     (define stmt `(assign ,(first bind) ,(first (rest bind))))
-     (define seq `(seq ,stmt ,tail))
-     seq]
-    [_ (list e tail)]))
+(define (ec_assign val var tail)
+  (match val
+    [(? symbol?)
+     (error "not implemented")]
+    [(? integer?)
+     `(seq (assign ,var ,val) ,tail)]
+    [`(read)
+     (error "not implemented")]
+    [`(let ([,new_var ,val]) ,body)
+     (error "not implemented")]
+    [`(,op ,args ...)
+     (error "not implemented")]))
+;    ;[(? symbol?) (your-code-here)]
+;    ;[(? integer?) `(seq (assign ,x ,e) ,k)]
+;    ;[`(read) (your-code-here)]
+;    [(list bind)
+;     (define stmt `(assign ,(first bind) ,(first (rest bind))))
+;     (define seq `(seq ,stmt ,tail))
+;     seq]
+;    [_ (list e tail)]))
 
 ; atomic test cases
+(check-equal? (ec_tail 3) (list 'return 3))
 
 ; simple let case
 (check-equal? (ec_tail '(let ([x 2]) x)) '(seq (assign x 2) (return x)))
@@ -42,3 +63,5 @@
               '(seq (assign x 2)
                     (seq (assign y 1)
                          (return (+ x y)))))
+
+(check-equal? (ec_tail '(let ([x 2]) x)) '(seq (assign x 2) (return x)))
