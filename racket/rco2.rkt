0b48ecf3be2899a4bb1c6276a1e40bcd1b9442bd
John Vincent Tan <john.v.tan@gmail.com>
2019-07-29 12:49:25 -0400

    handle op args in rco-exp and fix tests for rco-arg

--- racket/rco2.rkt
+++ racket/rco2.rkt
@@ -3,39 +3,64 @@
 (require rackunit)
 
 ; Given an expr in R1, return an expr in R1 without any complex subexpressions
-(define (rco-exp e) ; returns expr
-  (match e
+(define (rco-exp exprs) ; returns expr
+  (match exprs
     ; handle base cases
-    [(or (? symbol?) (? integer?) '(read)) e]
+    [(or (? symbol?) (? integer?) '(read)) exprs]
     ; ??? in rust we add this binding to the alist but maybe that's not right...
-    [(list 'let ([var val]) body) 
+    [(list 'let (list [list var val]) body)
      (displayln (list var val body)) 
      (error "rco-exp let case")]
     ; this case should call rco-arg on each of the args
     ; then build a new expr with bindings from the rco-arg calls
-    [(list op args ...) (error "rco-exp op args case")]))
+    [(list op args ...) 
+     (define-values [syms alists]
+       (for/lists (l1 l2) 
+                ([e exprs])
+       (rco-arg e)))
+     (displayln syms)
+     (displayln alists)]))
 
  ; Given an expr in R1, return a temp symbol name and an alist mapping from the symbol name to an expr in R1
-(define (rco-arg e) ; returns expr, alist
-  (match e
+(define (rco-arg exprs) ; returns expr, alist
+  (match exprs
     ; handle simple base cases
-    [(or (? symbol?) (? integer?) '(read)) (list e)]
-    ; 
-    [(list 'let ([var val]) body) (error "rco-exp let case")]
+    [(or (? symbol?) (? integer?) '(read)) (values exprs '())]
+    ; TODO let case should bind var to val in an alist and evaluate the body somehow 
+    [(list 'let (list [list var val]) body) (error "rco-arg let case")]
     [(list op args ...) (let ([tmp-name (gensym 'tmp)])
                           (values tmp-name
-                                  (list tmp-name (rco-exp e))))]))
+                                  (list tmp-name (rco-exp exprs))))]))
+
+; TEST HELPERS
+(define make-list-from-vals (λ (a b) (list a b)))
+
+(define (verify-rco-arg-output-is-empty given)
+  (check-match 
+    (call-with-values (λ () (rco-arg given)) make-list-from-vals) 
+    (list given (? empty?))))
+
+(define (verify-rco-arg-output given expect)
+  (check-match 
+    (call-with-values (λ () (rco-arg given)) make-list-from-vals) 
+    (list (? symbol? s) (list (? symbol? s) expect))))
+
+ 
 
 ; TEST CASES
 ; ATOMS should stay simple rco-exp
 (check-equal? (rco-exp 2) 2)
 (check-equal? (rco-exp '+) '+)
 ; ATOMS should stay simple rco-arg
-(check-equal? (rco-arg 2) (list 2))
-(check-equal? (rco-arg '+) (list '+))
+(verify-rco-arg-output-is-empty 2)
+(verify-rco-arg-output-is-empty '+)
+
+
+; OPERATIONS should get simplied by rco-arg
+(verify-rco-arg-output '(+ 2 2) '(+ 2 2))
 
 (displayln "yes")
-(rco-arg (let ([x 1]) x))
+;(rco-arg '(let ([x 1]) x))
 ;
 ;; BAD exprs rco-exp
 ;(check-equal? (rco-exp (list 2)) "panic!")
@@ -50,9 +75,7 @@
 ;(check-equal? (rco-exp '(let ([x 2]) x)) '(let ([x 2]) x))
 ;(check-equal? (rco-exp (list 'read)) '(read))
 ;
-(define make-list-from-vals (λ (a b) (list a b)))
-(check-match 
-  (call-with-values (λ () (rco-arg '(+ 2 2))) make-list-from-vals) 
-  (list (? symbol? s) (list (? symbol? s) '(+ 2 2))))
+
+ 
 
 (displayln "tests finished")
