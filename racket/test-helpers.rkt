c25f5ba57ff493b981e69581e809fbe25058e2d0
mfekadu <mike.fekadu@gmail.com>
2019-08-01 16:26:05 -0400

    add test helpers & brief about "why code coverage"
    new helpers are
    * check-fail
    * check-fail-with-name
    
    import into any rkt file via
    (require "test-helpers.rkt")
    
    the test helpers themselves are extensively tested. (full code coverage on DrRacket)
    
    Sometimes I wonder if aiming for "full code coverage in DrRacket" is a bad thing. And well I see that it could be an insufficient heuristic for confirming whether code is sufficiently tested. However, I find that it's a good way to overcome the seemingly daunting activation energy of writing test cases. Simply put, some tests are better than none, right? So, while DrRacket's code coverage tool does not necessarily guarantee that I've properly tested the code, it does at least provide small / achievable goal for just getting started with test cases. The process of writing the tests can then spark ideas for even better tests that the code coverage tool would not explicitly show.
    
    \end-stream-of-consciousness
    
    haha xD

---
+++ racket/test-helpers.rkt
@@ -0,0 +1,43 @@
+#!/usr/bin/racket
+#lang racket
+(require rackunit)
+(provide check-fail check-fail-with-name)
+
+; https://docs.racket-lang.org/rackunit/api.html#%28def._%28%28lib._rackunit%2Fmain..rkt%29._check-exn%29%29
+; given a thunk/lambda/procedure/funciton... make sure it fails
+(define (check-fail thunk) (check-exn exn:fail? thunk))
+
+; test check-fail
+(check-true (void? (check-fail (λ () (error)))))
+
+
+; setup namespace for eval
+; https://stackoverflow.com/a/37246112
+(current-namespace (make-base-namespace))
+
+; given the symbol name of a function
+; and the given_arg to pass into the function
+; make sure it fails and the error message contains the fun_name
+(define (check-fail-with-name fun_name fun_proc given_arg)
+  (cond [(symbol? fun_name)
+         (define regex_str (string-append (symbol->string fun_name) ".*"))
+         (check-exn (regexp regex_str) (λ () (fun_proc given_arg)))]
+        [else
+         (error 'check-fail-with-name "bad fun_name: ~v" fun_name)]))
+; test check-fail-with-name
+(check-true (void? (check-fail-with-name 'error error 'error)))
+
+; test bad args
+(check-fail (λ () (check-fail-with-name 32 42)))
+
+; test it catches diff name failure
+(define fn_err_with_diff_name (λ (bar) (error 'foo "wow ~v" bar)))
+(check-fail (λ () (fn_err_with_diff_name 'foo))) ; make sure it err by itself
+(define thunk (λ () (check-fail-with-name 'fn_err_with_diff_name fn_err_with_diff_name "given_arg")))
+(check-true (void? (check-fail thunk))) ; test case
+
+; test it catches allows same name failure
+(define fn_err_same_name (λ (bar) (error 'fn_err_same_name "wow ~v" bar)))
+(check-fail (λ () (fn_err_same_name 'foo))) ; make sure it err by itself
+(define res (check-fail-with-name 'fn_err_same_name fn_err_same_name 'foo))
+(check-true (void? res)) ; test case
