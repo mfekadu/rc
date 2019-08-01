5ba230da0144b4b82cdcbeb51763068c8742dd2b
John Vincent Tan <john.v.tan@gmail.com>
2019-08-01 16:58:12 -0400

    failing handle-tail test. instructions need to be more nested?

--- racket/select-instructions.rkt
+++ racket/select-instructions.rkt
@@ -5,7 +5,8 @@
 (define (select-instructions c0-prog)
   (match c0-prog
     [`(program ,locals (,label ,tail)) 
-      `(program ,locals (,label (,handle-tail ,tail)))]
+      (displayln tail)
+      `((.globl ,label) ,locals (,label ,(handle-tail tail)))]
     [_ (error "oh no")])) 
 
 ; Given a C0 tail (sequence or return), call handle-stmt on statement and itself(?) on tail
@@ -15,12 +16,13 @@
       ; treat return as an assign to rax
       ; then do a jump to the conclusion
       ; .... where is conclusion? is it labeled?
-      `(movq (handle-arg expr) (reg rax))]
+      `(movq ,(handle-arg expr) (reg rax))]
     [`(seq ,stmt ,new-tail) 
+      (displayln "here")
       ; handle the stmt and recursively call handle-tail on the tail
       (define new-stmt-instr (handle-stmt stmt))
       (define new-tail-instr (handle-tail new-tail))
-      100]
+      (append new-stmt-instr new-tail-instr)]
     [_ (error "uhhhhhhhhhhhhhhhhhhhhhh")]))
 
 ; Given a C0 arg (int or var), emit an x86_0 arg (int 2) or (reg register)
@@ -44,7 +46,7 @@
       (define x86-var (handle-arg lhs))
       `((movq ,(handle-arg arg) ,x86-var) 
         (negq ,x86-var))]
-    [`(assign ,(? symbol? lhs) ,val) `(movq ,(handle-arg val) ,(handle-arg lhs))]
+    [`(assign ,(? symbol? lhs) ,val) `((movq ,(handle-arg val) ,(handle-arg lhs)))]
     [_ (error "plsno")]))
 
 (define given1 '((movq (int 32) (var x)) (addq (int 32) (var x))))
@@ -60,3 +62,15 @@
 
 (define given5 '(assign x 5))
 (check-equal? (handle-stmt given5) `(movq (int 5) (var x)))
+
+; testing handle-tail
+(define given6 '(seq (assign x 6) (return x)))
+(check-equal? (handle-tail given6) '((movq (int 6) (var x)) (movq (var x) (reg rax))))
+
+; tests for handle-tail
+;(define given2-prog `(program (x) (start (seq (assign x (+ 10 x)) (return x)))))
+;(check-equal? (select-instructions given2-prog) `((.globl start) (x)
+;                                                  (start
+;                                                    (movq (int 10) (var x))
+;                                                    (addq (var x) (var x))
+;                                                    (mov (var x) (reg rax)))))
