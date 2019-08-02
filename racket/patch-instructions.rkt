18d60c83884d40e75c71bd00791b44d7b9f4097f
mfekadu <mike.fekadu@gmail.com>
2019-08-02 17:10:52 -0400

    Create patch-instructions.rkt
    Co-Authored-By: johnvtan <johnvtan@users.noreply.github.com>

---
+++ racket/patch-instructions.rkt
@@ -0,0 +1,25 @@
+#!/usr/bin/racket
+#lang racket
+(require rackunit)
+(require racket/contract)
+(require "test-helpers.rkt") ; for check-fail and check-fail-with-name
+
+; Given an x86 program, break up all instructions of the form
+;               op (deref rbp v1) (deref rbp v2)
+; into 
+;               mov (deref rbp v1) (reg rax)
+;               op (deref rbp v2) (reg rax)
+;               mov (reg rax) (deref rbp v1)
+;
+; There's a potential optimization here, e.g.:
+;     addq (deref rbp -8) (deref rbp -16)
+;     addq (deref rbp -8) (deref rbp -24)
+; Would become (unoptimized):
+;     mov (deref rbp -8) (reg rax)
+;     addq (deref rbp -16) (reg rax)
+;     mov (reg rax) (deref rbp -8)
+;     mov (deref rbp -8) (reg rax)
+;     addq (deref rbp -24) (reg rax)
+;     mov (reg rax) (deref rbp -8)
+(define (patch-instructions x86-prog)
+  )
