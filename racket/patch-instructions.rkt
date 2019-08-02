ec60fb7000c302e55c875bc2ba01ad5d2f0c7da3
John Vincent Tan <john.v.tan@gmail.com>
2019-08-02 17:26:39 -0400

    starting to patch instructions

--- racket/patch-instructions.rkt
+++ racket/patch-instructions.rkt
@@ -11,7 +11,7 @@
 ;               op (deref rbp v2) (reg rax)
 ;               mov (reg rax) (deref rbp v1)
 ;
-; There's a potential optimization here, e.g.:
+; TODO There's a potential OPTIMIZATION here, e.g.:
 ;     addq (deref rbp -8) (deref rbp -16)
 ;     addq (deref rbp -8) (deref rbp -24)
 ; Would become (unoptimized):
@@ -22,4 +22,10 @@
 ;     addq (deref rbp -24) (reg rax)
 ;     mov (reg rax) (deref rbp -8)
 (define (patch-instructions x86-prog)
-  )
+  (match x86-prog 
+    [`(program ,locals (,label ,instrs))
+      `(program ,locals (,label ,(replace-invalid-instrs instrs)))]
+    [_ (error "Malformed input program to patch-instructions ~s " x86-prog)]))
+
+(define (replace-invalid-instrs instrs)
+  (match instrs))
