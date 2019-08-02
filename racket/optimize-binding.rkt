9c731c3764566b677d8501f4a661b63683241179
mfekadu <mike.fekadu@gmail.com>
2019-08-01 21:37:14 -0400

    Update optimize-binding.rkt
    notice that hash-set order is not preserved
    
    ```
    > (hash 'x1 20)
    (make-immutable-hash (list (cons 'x1 20)))
    > (displayln (hash 'x1 20))
    #hash((x1 . 20))
    > (displayln (hash-set (hash 'x1 20) 'tmp '(+ x1 22)))
    #hash((tmp . (+ x1 22)) (x1 . 20))
    > (displayln (hash-set (hash-set (hash 'x1 20) 'tmp '(+ x1 22)) 'y 'tmp))
    #hash((tmp . (+ x1 22)) (x1 . 20) (y . tmp))
    > (displayln (hash-set (hash-set (hash 'x1 20) 'tmp '(+ x1 22)) 'x1 'tmp))
    #hash((tmp . (+ x1 22)) (x1 . tmp))
    > (displayln (hash-set (hash-set (hash 'x1 20) 'tmp '(+ x1 22)) 'x0 'tmp))
    #hash((tmp . (+ x1 22)) (x0 . tmp) (x1 . 20))
    > (displayln (hash-set (hash-set (hash 'x1 20) 'tmp '(+ x1 22)) 'foo 'tmp))
    #hash((foo . tmp) (tmp . (+ x1 22)) (x1 . 20))
    >
    ```
    
    also some printouts.....
    beginning to think that the two non-opimized examples in the purpose statement are the same... or in other words. the first example is a further nested version of the second example because the 'y variable in the first example gets bound to the value of 'tmp but then it remains in a state where it looks like the second example such that it could get further optimized to look like the final optimized solution of the second example..... hmmm.... might end up changing the test cases soon
    
    ```
    Welcome to DrRacket, version 7.3 [3m].
    Language: racket, with test coverage [custom]; memory limit: 128 MB.
    (let ((x1 20)) (let ((tmp (+ x1 22))) (let ((y tmp)) y)))
    #hash((tmp . (+ x1 22)) (x1 . 20) (y . tmp))
    --------------------
    . FAILURE
    name:       check-equal?
    location:   optimize-binding.rkt:50:0
    actual:     '(let ((y tmp)) y)
    expected:   '(let ((x1 20)) (let ((y (+ x1 22))) y))
    --------------------
    (let ((x1 20)) (let ((tmp (+ x1 22))) tmp))
    #hash((tmp . (+ x1 22)) (x1 . 20))
    --------------------
    . FAILURE
    name:       check-equal?
    location:   optimize-binding.rkt:55:0
    actual:     '(let ((tmp (+ x1 22))) tmp)
    expected:   '(let ((x1 20)) (+ x1 22))
    --------------------
    ```

--- racket/optimize-binding.rkt
+++ racket/optimize-binding.rkt
@@ -23,18 +23,35 @@
 ; which becomes .........(let [[x1 20]] (let [[y (+ x1 22)]] y))
 ; OR non-optimized is ...(let [[x1 20]] (let [[tmp (+ x1 22)]] tmp))
 ; which becomes .........(let [[x1 20]] (+ x1 22))
+; NOTE: only use `optimize-binding` after `uniquify`
+;       because otherwise weird stuff could happen when we hash-set?
+; NOTE; only use `optimize-binding` after remove-complex-opera
+;       becuase it assumes that the ,val of each binding is already simple expr
+;       also because it assumes the ,val is not a binding as well (i.e. simple expr)
 (define (optimize-binding let-expr unused-vars)
   (match let-expr
     [`(let [[,(? symbol? var) ,val]] ,body)
      #:when (is-let? body)
-     (optimize-binding body unused-vars)]
+     (define new-hash (hash-set unused-vars var val))
+     (optimize-binding body new-hash)]
+    [`(let [[,(? symbol? var) ,val]] ,body)
+     ; if the above didn't match, then ,body is not let
+     (define new-hash (hash-set unused-vars var val))
+     (displayln new-hash)
+     let-expr]
+    ; anything else should stay the same
     [_ let-expr]))
 
 
+; test anything else should stay the same
+(check-equal? (optimize-binding '(+ 2 2) (hash)) '(+ 2 2))
 
+(displayln '(let [[x1 20]] (let [[tmp (+ x1 22)]] (let [[y tmp]] y))))
 (check-equal? (optimize-binding '(let [[x1 20]] (let [[tmp (+ x1 22)]] (let [[y tmp]] y)))
                   (hash))
               '(let [[x1 20]] (let [[y (+ x1 22)]] y)))
+
+(displayln '(let [[x1 20]] (let [[tmp (+ x1 22)]] tmp)))
 (check-equal? (optimize-binding '(let [[x1 20]] (let [[tmp (+ x1 22)]] tmp))
                   (hash))
               '(let [[x1 20]] (+ x1 22)))
