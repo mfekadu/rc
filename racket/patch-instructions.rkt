1b77e7cad4e1259fe4e2955e36eeb207bdaa4378
mfekadu <mike.fekadu@gmail.com>
2019-08-05 22:39:55 -0400

    quick pass patch-instrs 3 tests
    Co-Authored-By: johnvtan <johnvtan@users.noreply.github.com>

--- racket/patch-instructions.rkt
+++ racket/patch-instructions.rkt
@@ -24,8 +24,40 @@
 (define (patch-instructions x86-prog)
   (match x86-prog 
     [`(program ,locals (,label ,instrs))
-      `(program ,locals (,label ,(replace-invalid-instrs instrs)))]
+     ; instrs is a list of instructions
+      `(program ,locals (,label ,(rec-replace-invalid-instrs instrs)))]
     [_ (error "Malformed input program to patch-instructions ~s " x86-prog)]))
 
+(define (rec-replace-invalid-instrs instrs)
+  (cond [(empty? instrs) (error 'rec-replace-invalid-instrs "bad empty instr")]
+        [else (append (replace-invalid-instrs (first instrs))
+                      (rec-replace-invalid-instrs (rest instrs)))]))
+
 (define (replace-invalid-instrs instrs)
-  (match instrs))
+  (match instrs
+    [`(movq (deref rbp ,offset1) (deref rbp ,offset2))
+     `((movq (deref rbp ,offset1) (reg rax))
+       (movq (reg rax) (deref rbp ,offset2)))]
+    [`(,op (deref rbp ,offset1) (deref rbp ,offset2))
+     ;LIST OF INSTRS???
+     `((movq (deref rbp ,offset1) (reg rax))
+      (addq (deref rbp ,offset2) (reg rax))
+      (movq (reg rax) (deref rbp ,offset1)))
+     ]
+    [_ `(,instrs)]))
+
+
+(define given '(addq (deref rbp v1) (deref rbp v2)))
+(define expect '((movq (deref rbp v1) (reg rax))
+                 (addq (deref rbp v2) (reg rax))
+                 (movq (reg rax) (deref rbp v1))))
+(check-equal? (replace-invalid-instrs given) expect)
+
+(define given2 '(addq (int 2) (deref rbp v2)))
+(define expect2 `(,given2))
+(check-equal? (replace-invalid-instrs given2) expect2)
+
+(define given3 '(movq (deref rbp v1) (deref rbp v2)))
+(define expect3 '((movq (deref rbp v1) (reg rax))
+                 (movq (reg rax) (deref rbp v2))))
+(check-equal? (replace-invalid-instrs given3) expect3)
