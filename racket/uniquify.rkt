6b17067a4a93a03b2afa7e7828c48bb9dfd01a0f
John Vincent Tan <john.v.tan@gmail.com>
2019-08-07 12:10:08 -0400

    uniquify passing tests

--- racket/uniquify.rkt
+++ racket/uniquify.rkt
@@ -4,21 +4,55 @@
 (require racket/contract)
 (require "test-helpers.rkt") ; for check-fail and check-fail-with-name
 
+; looks up s in alist. Returns the mapped symbol if it exists, otherwise throws an error
+(define (alist-get s alist)
+  (define entry (assoc s alist))
+  (if (eq? entry #f)
+    (error "Syntax error: symbol ~s is undefined" s)
+    (cadr entry)))
+
+; Generates a new symbol for s. If it exists in the alist, update it, otherwise make a new entry
+; in the alist.
+(define (alist-update s alist)
+  (define new-sym (gensym s))
+  (define entry (assoc s alist))
+  (if (eq? entry #f)
+    ; if s not in alist, just append the new mapping
+    (cons `(,s ,new-sym) alist)
+    ; if s in alist, remove the old mapping and append the new mapping
+    (cons `(,s ,new-sym) (remove entry alist))))
+
 (define (uniquify-exp alist)
   (lambda (e)
     (match e
-      [(? symbol?) '()]
+      ; Look up the symbol in the alist. If it exists, return the mapping in the alist.
+      ; Otherwise, throw an error because the symbol wasn't defined 
+      [(? symbol? s) (alist-get s alist)]
       [(? integer?) e]
-      [`(let ([,x ,e]) ,body) '()]
+      [`(let ([,var ,val]) ,body) 
+        ; uniquify val expr with old alist
+        (define uniquified-val ((uniquify-exp alist) val))
+
+        ; update the alist to reflect the newly defined var
+        ; if it exists, update the mapping
+        (define new-alist (alist-update var alist))
+
+        (define new-var (alist-get var new-alist))
+
+        ; then evaluate the body with the updated alist
+        (define uniquified-body ((uniquify-exp new-alist) body))
+
+        ; then return the whole uniquified let expr
+        `(let ([,new-var ,uniquified-val]) ,uniquified-body)]
       [`(,op ,es ...)
        `(,op ,@(for/list ([e es]) ((uniquify-exp alist) e)))]
       [_ (error "Malformed expression given to uniquify-exp: ~s" e)])))
 
-(define (uniquify alist)
+(define (uniquify)
   (lambda (e)
     (match e
       [`(program ,info ,e)
-       `(program ,info ,((uniquify-exp alist) e))]
+       `(program ,info ,((uniquify-exp '()) e))]
       [_ (error "Malformed program given to uniquify: ~s" e)])))
 
 ; tests for uniquify-exp
@@ -38,3 +72,12 @@
               `(let ([,(? symbol? s1) 1]) 
                  (let ([,(? symbol? s2) ,s1])
                    (+ ,s2 ,s2))))
+
+; should fail since x is not defined
+(define given4 '(+ x 2))
+(check-fail (lambda () (uniquify-exp-func given4)))
+
+(define given5 '(let ([x 5]) (+ y 3)))
+(check-fail (lambda () (uniquify-exp-func given5)))
+
+(displayln "tests pass")
