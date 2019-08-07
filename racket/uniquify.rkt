af97273b9664ce36fa59602dc8aa7cf1047469d3
John Vincent Tan <john.v.tan@gmail.com>
2019-08-07 11:01:40 -0400

    Added tests to uniquify

--- racket/uniquify.rkt
+++ racket/uniquify.rkt
@@ -9,12 +9,26 @@
     (match e
       [(? symbol?) '()]
       [(? integer?) e]
-      ['(let ([,x ,e]) ,body) '()]
-      ['(,op ,es ...)
-       '(,op ,@(for/list ([e es]) ((uniquify-exp alist) e)))])))
+      [`(let ([,x ,e]) ,body) '()]
+      [`(,op ,es ...)
+       `(,op ,@(for/list ([e es]) ((uniquify-exp alist) e)))])))
 
 (define (uniquify alist)
   (lambda (e)
     (match e
-      ['(program ,info ,e)
-       '(program ,info ,((uniquify-exp alist) e))])))
+      [`(program ,info ,e)
+       `(program ,info ,((uniquify-exp alist) e))])))
+
+(define uniquify-exp-func (uniquify-exp '()))
+
+(define given1 '(+ 2 2))
+(define expect1 '(+ 2 2))
+(check-equal? (uniquify-exp-func given1) expect1)
+
+(define given2 '(let ([x 2]) (+ 2 x)))
+(define expect2 '(let ([x1 2]) (+ 2 x1)))
+(check-equal? (uniquify-exp-func given2) expect2)
+
+(define given3 '(let ([x 1]) (let ([x x]) (+ x x))))
+(define expect3 '(let ([x1 1]) (let ([x2 x1]) (+ x2 x2))))
+(check-equal? (uniquify-exp-func given3) expect3)
