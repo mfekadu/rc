7c3aa30db76a94ac2da24bcf6a3141c817420c1e
mfekadu <mike.fekadu@gmail.com>
2019-08-07 13:55:25 -0400

    fixed the offset issue and fully tested offsets for patch-instr

--- racket/patch-instructions.rkt
+++ racket/patch-instructions.rkt
@@ -52,10 +52,13 @@
 
 (define (replace-invalid-instrs instrs)
   (match instrs
+    [`(,op ,arg (deref rbp ,offset2))
+     #:when (not (is-neg-mult-8? offset2))
+     (error 'replace-invalid-instrs "bad offset: {{ ~v }}" offset2)]
     [`(,op (deref rbp ,offset1) (deref rbp ,offset2))
      #:when (or (not (is-neg-mult-8? offset1)) (not (is-neg-mult-8? offset2)))
      (error 'replace-invalid-instrs "one or more bad offsets: {{ ~v }} OR {{ ~v }}" offset1 offset2)]
-    [`(movq (deref rbp ,offset1 (deref rbp ,offset2)))
+    [`(movq (deref rbp ,offset1) (deref rbp ,offset2))
      `((movq (deref rbp ,offset1) (reg rax))
        (movq (reg rax) (deref rbp ,offset2)))]
     [`(,op (deref rbp ,offset1) (deref rbp ,offset2))
@@ -66,17 +69,29 @@
     [_ `(,instrs)]))
 
 
-(define given '(addq (deref rbp v1) (deref rbp v2)))
-(define expect '((movq (deref rbp v1) (reg rax))
-                 (addq (deref rbp v2) (reg rax))
-                 (movq (reg rax) (deref rbp v1))))
-(check-fail (λ () (replace-invalid-instrs given) expect))
+(define given_bad_offset '(addq (deref rbp v1) (deref rbp v2)))
+(check-fail (λ () (replace-invalid-instrs given_bad_offset)))
 
-(define given2 '(addq (int 2) (deref rbp v2)))
+(define given_bad_offset2 '(addq (deref rbp -8) (deref rbp 16)))
+(check-fail (λ () (replace-invalid-instrs given_bad_offset2)))
+
+(define given_bad_offset3 '(addq (int 2) (deref rbp v2)))
+(check-fail (λ () (replace-invalid-instrs given_bad_offset3)))
+
+(define given_bad_offset4 '(movq (deref rbp 8) (deref rbp -16)))
+(check-fail (λ () (replace-invalid-instrs given_bad_offset4)))
+
+(define given '(addq (deref rbp -8) (deref rbp -16)))
+(define expect '((movq (deref rbp -8) (reg rax))
+                 (addq (deref rbp -16) (reg rax))
+                 (movq (reg rax) (deref rbp -8))))
+(check-equal? (replace-invalid-instrs given) expect)
+
+(define given2 '(addq (int 2) (deref rbp -8)))
 (define expect2 `(,given2))
 (check-equal? (replace-invalid-instrs given2) expect2)
 
-(define given3 '(movq (deref rbp 8) (deref rbp 16)))
-(define expect3 '((movq (deref rbp v1) (reg rax))
-                 (movq (reg rax) (deref rbp v2))))
+(define given3 '(movq (deref rbp -8) (deref rbp -16)))
+(define expect3 '((movq (deref rbp -8) (reg rax))
+                 (movq (reg rax) (deref rbp -16))))
 (check-equal? (replace-invalid-instrs given3) expect3)
