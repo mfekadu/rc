652a21b327af2b58dd759e0608773f0f1041a0e7
John Vincent Tan <john.v.tan@gmail.com>
2019-08-12 17:05:26 -0400

    Added readme with instructions

---
+++ racket/README.md
@@ -0,0 +1,14 @@
+# Description
+This is a racket to x86 compiler written in racket. It follows the approach outlined by the book `Essentials of Compilation` by Jeremy Siek and Ryan Newton.
+
+# Compiling a file 
+```
+usage: build.py [-h] [-o OUTPUT] filename
+```
+
+# Running tests
+All tests are in the `testing` directory.
+
+```
+./run_tests.sh
+```
--- racket/run_tests.sh
+++ racket/run_tests.sh
@@ -1,25 +1,25 @@
-#!/bin/bash
+#./testing/bi./testing/bash
 
 echo -e "\nRunning assign-homes tests..."
-./assign-homes-tests.rkt
+./testing/assign-homes-tests.rkt
 
 echo -e "\nRunning ec-tests..."
-./ec-tests.rkt
+./testing/ec-tests.rkt
 
 echo -e "\nRunning patch instructions tests..."
-./patch-instructions-tests.rkt
+./testing/patch-instructions-tests.rkt
 
 echo -e "\nRunning print-x86 tests..."
-./print-x86-tests.rkt
+./testing/print-x86-tests.rkt
 
 echo -e "\nRunning rco tests..."
-./rco-tests.rkt
+./testing/rco-tests.rkt
 
 echo -e "\nRunning select instructions tests..."
-./select-instructions-tests.rkt
+./testing/select-instructions-tests.rkt
 
 echo -e "\nRunning uncover locals tests..."
-./uncover-locals-tests.rkt
+./testing/uncover-locals-tests.rkt
 
 echo -e "\nRunning uniquify tests..."
-./uniquify-tests.rkt
+./testing/uniquify-tests.rkt
