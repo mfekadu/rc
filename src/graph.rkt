c2a95d279e4449e0e78f6c35d33f02c3c66f8680
John Vincent Tan <john.v.tan@gmail.com>
2019-08-14 17:36:09 -0400

    added graph-get-edges

--- src/graph.rkt
+++ src/graph.rkt
@@ -1,7 +1,8 @@
 #!/usr/local/bin/racket
 #lang racket
 
-(provide graph-contains)
+(provide graph-contains?)
+(provide graph-get-edges)
 (provide graph-add-edge)
 
 ; simple graph library for make-interference pass
@@ -11,15 +12,25 @@
 ; graphs are undirected
 
 ; return true if v in g else false
-(define (graph-contains g v)
+(define (graph-contains? g v)
   (cond
     [(empty? g) #f]
     [(equal? (car (first g)) v) #t]
-    [else (graph-contains (rest g) v)]))
+    [else (graph-contains? (rest g) v)]))
+
+(define (graph-get-edges g v)
+  (cond 
+    ; TODO - should this error if we get an empty list?
+    [(empty? g) (error "ERROR tried to get edges from vertex not in graph")]
+    [(equal? (car (first g)) v) (cdr (first g))]
+    [else (graph-get-edges (rest g) v)]))
 
 (define (graph-add-edge g v1 v2)
-  
   ; inner function specifically adds edge from v1 to v2
   (define (graph-add-edge-one-way g v1 v2)
-    (displayln "unimplemented"))
-  (displayln "unimplemented"))
+    (cond
+      [(graph-contains? g v1) (displayln "unimplmeneted")]
+      [else (displayln "unimplmented")]))
+
+  (define after-v1-added (graph-add-edge-one-way g v1 v2))
+  (graph-add-edge-one-way g v2 v1))
--- testing/graph-tests.rkt
+++ testing/graph-tests.rkt
@@ -8,8 +8,16 @@
 (define fc-graph '((x . (y z)) (y . (x z)) (z . (x y))))
 (define empty-graph '())
 
-(check-equal? (graph-contains fc-graph 'x) #t)
-(check-equal? (graph-contains fc-graph 'a) #f)
-(check-equal? (graph-contains empty-graph 'x) #f)
+; graph-contains? tests
+(check-equal? (graph-contains? fc-graph 'x) #t)
+(check-equal? (graph-contains? fc-graph 'z) #t)
+(check-equal? (graph-contains? fc-graph 'a) #f)
+(check-equal? (graph-contains? empty-graph 'x) #f)
+
+; graph-get-edges tests
+(check-equal? (graph-get-edges fc-graph 'x) '(y z))
+(check-equal? (graph-get-edges fc-graph 'z) '(x y))
+(check-fail (lambda () (graph-get-edges fc-graph 'a)))
+(check-fail (lambda () (graph-get-edges empty-graph 'z)))
 
 (displayln "Graph tests finished running")
