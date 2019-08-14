1c2c98f268e41ef1b8d991e44f3cad7e91c1dcf5
John Vincent Tan <john.v.tan@gmail.com>
2019-08-14 18:22:05 -0400

    Adding graph edges is OK

--- src/graph.rkt
+++ src/graph.rkt
@@ -4,10 +4,13 @@
 (provide graph-contains?)
 (provide graph-get-edges)
 (provide graph-add-edge)
+(provide graph-add-edge-one-way)
+(provide graph-insert)
+(provide graph-remove)
 
 ; simple graph library for make-interference pass
 
-; graphs are alists that look like ((x (a b)) (a (x)) (b (x))), where the first element is a vertex and the second
+; graphs are alists that look like ((x . (a b)) (a . (x)) (b . (x))), where the first element is a vertex and the second
 ; element is a list of edges which the vertex is connected to.
 ; graphs are undirected
 
@@ -25,12 +28,34 @@
     [(equal? (car (first g)) v) (cdr (first g))]
     [else (graph-get-edges (rest g) v)]))
 
-(define (graph-add-edge g v1 v2)
-  ; inner function specifically adds edge from v1 to v2
-  (define (graph-add-edge-one-way g v1 v2)
-    (cond
-      [(graph-contains? g v1) (displayln "unimplmeneted")]
-      [else (displayln "unimplmented")]))
+; Creates new element for v in g with edges e
+; Does this blindly. If g already contains an element with v, then there will be duplicates.
+(define (graph-insert g v e)
+  (cons (cons v e) g))
 
+; removes first element for vertex v in graph g
+; does not remove any other references to v that may still exist in g
+(define (graph-remove g v)
+  (remove v g (lambda (a b) (equal? a (car b)))))
+
+; adds edge from v1 to v2, but not the other way around
+(define (graph-add-edge-one-way g v1 v2)
+  (cond
+    ; if v1 already in g, just append v2 to the existing element if necessary
+    [(graph-contains? g v1)
+      (define v1-edges (graph-get-edges g v1))
+      (cond
+        ; if v2 is already in the list of v1's edges, return the original graph
+        [(member v2 v1-edges) g]
+        ; otherwise remove v1's element from the existing graph, append v2 to v1's edges, and append the new v1 element
+        ; to the graph
+        [else
+          (define old-edges (graph-get-edges g v1))
+          (define new-graph (graph-remove g v1))
+          (graph-insert new-graph v1 (cons v2 old-edges))])]
+    ; v1 not in g, create an entirely new element
+    [else (graph-insert g v1 (list v2))]))
+
+(define (graph-add-edge g v1 v2)
   (define after-v1-added (graph-add-edge-one-way g v1 v2))
-  (graph-add-edge-one-way g v2 v1))
+  (graph-add-edge-one-way after-v1-added v2 v1))
--- testing/graph-tests.rkt
+++ testing/graph-tests.rkt
@@ -20,4 +20,26 @@
 (check-fail (lambda () (graph-get-edges fc-graph 'a)))
 (check-fail (lambda () (graph-get-edges empty-graph 'z)))
 
+; graph-insert tests
+(check-equal? (graph-insert empty-graph 'x '(y z)) '((x . (y z))))
+
+; graph-remove test
+(check-equal? (graph-remove fc-graph 'z) '((x . (y z)) (y . (x z))))
+(check-equal? (graph-remove fc-graph 'a) fc-graph)
+(check-equal? (graph-remove empty-graph 'x) empty-graph)
+
+; graph-add-edge-one-way tests
+
+; add existing edge
+(check-equal? (graph-add-edge-one-way fc-graph 'x 'y) fc-graph)
+
+; add edge to completely new node
+(check-equal? (graph-add-edge-one-way fc-graph 'a 'z) '((a . (z)) (x . (y z)) (y . (x z)) (z . (x y))))
+
+; add new edge to existing node
+(check-equal? (graph-add-edge-one-way fc-graph 'x 'a) '((x . (a y z)) (y . (x z)) (z . (x y))))
+
+; add new node to empty list
+(check-equal? (graph-add-edge-one-way empty-graph 'x 'y) '((x . (y))))
+
 (displayln "Graph tests finished running")
