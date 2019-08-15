d25d95b4212a77eea54e7f00602464766749231d
mfekadu <mike.fekadu@gmail.com>
2019-08-15 17:15:09 -0400

    update helper1 to get-live-after-sets
    function will somehow combine Sets within a List after doing the right calculation using the results from the other helper functions.
    
    info about Set datatype...
    
    https://docs.racket-lang.org/reference/sets.html

--- src/uncover-live.rkt
+++ src/uncover-live.rkt
@@ -10,20 +10,39 @@
 
 ; given a list of instructions
 ; and an initial live-after set (typically empty)
-; returns the list of live-after sets.
-(define (helper1 foo bar)
-  (error 'helper1 "not yet implemented"))
+; returns the list of live-after Set (i.e. the Set datatype in Racket).
+(define (get-live-after-sets instrs init-set)
+  (match instrs
+    [(not (? set? init-set))
+     (error 'get-live-after-sets "bad init-set ~v" init-set)]
+    [(? empty? instrs)
+     ; there are no variables live after the last instruction
+     ; so return the empty set
+     (set)]
+    [(list (? list? first-instr) rest-instrs ...)
+     (define V (get-vars first-instr))
+     (define R (get-read-vars first-instr))
+     (define W (get-write-vars first-instr))
+     "???"
+     ;(define Lafter (set
+     ;                set-subtract
+     ;                set-union
+     ]
+    [_ (error 'get-live-after-sets "bad instrs ~v" instrs)]))
 
 ; compute the set of variables that appear in an argument (of an instruction)
-(define (helper2 foo bar)
-  (error 'helper2 "not yet implemented"))0
+; return a list of all variables
+(define (get-vars instr)
+  (error 'helper2 "not yet implemented"))
 
 ; compute the variables read by an instruction, which corresponds to the R(k)
-(define (helper3 foo bar)
+; returns a list of symbols for variables that are read from
+(define (get-read-vars instr)
   (error 'helper3 "not yet implemented"))
 
 ; compute the variables written by an instruction which corresponds to W(k)
-(define (helper4 foo bar)
+; returns a list of symbols for variables that are written to
+(define (get-write-vars instr)
   (error 'helper4 "not yet implemented"))
 
 ; **************************************************
