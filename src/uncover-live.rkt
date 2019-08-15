4a363f2eb447870e4a4793f1f45a41f45234c606
mfekadu <mike.fekadu@gmail.com>
2019-08-15 16:41:15 -0400

    Update uncover-live.rkt

--- src/uncover-live.rkt
+++ src/uncover-live.rkt
@@ -3,6 +3,33 @@
 
 (provide uncover-live)
 
+
+; **************************************************
+; HELPERS
+; **************************************************
+
+; given a list of instructions
+; and an initial live-after set (typically empty)
+; returns the list of live-after sets.
+(define (helper1 foo bar)
+  (error 'helper1 "not yet implemented"))
+
+; compute the set of variables that appear in an argument (of an instruction)
+(define (helper2 foo bar)
+  (error 'helper2 "not yet implemented"))0
+
+; compute the variables read by an instruction, which corresponds to the R(k)
+(define (helper3 foo bar)
+  (error 'helper3 "not yet implemented"))
+
+; compute the variables written by an instruction which corresponds to W(k)
+(define (helper4 foo bar)
+  (error 'helper4 "not yet implemented"))
+
+; **************************************************
+; UNCOVER-LIVE
+; **************************************************
+
 ; given an x86_0 program
 ; computes the live-after sets as described in Ch3 of Siek et. al.
 ; returns the same program 
@@ -10,3 +37,7 @@
 ; inside the "block" clause of the x86_0 grammar (Siek et. al. pg 24)
 (define (uncover-live p)
   (error 'uncover-live "not yet implemented"))
+
+; **************************************************
+; TEST CASES (TODO: move into external file)
+; **************************************************
