c7cb9dcad6378f43e94a8098378889064bac94ee
John Vincent Tan <john.v.tan@gmail.com>
2019-08-15 22:32:53 -0400

    well the test went horribly wrong

--- src/make-interference.rkt
+++ src/make-interference.rkt
@@ -2,6 +2,8 @@
 #lang racket
 
 (require "graph.rkt")
+(provide make-interference)
+(provide interference-from-live)
 
 (define (make-interference p)
   (match p
@@ -11,17 +13,31 @@
 
 ; Given a list of variables that are grouped by when they are alive e.g. ((), (x), (x y), (x y z))
 ; Construct an interference graph that looks like ((x (y z)) (y (x z)) (z (x y)))
+; See pg 41 in Essentials of Compilation for details on building the interference graph
 (define (interference-from-live live-list instrs)
   ; iterate through both live-list and instrs
   (for/fold ([interference-graph '()])
             ([live-vars live-list]
              [instr instrs])
     (match instr
-      [`(addq (,_ ,source) (,_ ,dst))
+      ; If instr is an arithmetic operation, then add the edge (dst, var) for every var in the set L_after(k) unles var
+      ; == dst
+      ; TODO handle src/dst parsing better - probably want to check if it's a var or reg and not an int
+      [`(addq (,_ ,src) (,_ ,dst))
         (define new-edges (remove dst live-list))
-        (graph-add-multiple-edges interference-graph dst live-list)]
-      [`(movq ,src ,dst) (error "unimplemented movq")]
+        (graph-add-multiple-edges interference-graph dst new-edges)]
+
+      ; If instr is a move, then add the edge (dst, var) for every v in L_after(k) unless v == dst or v == src
+      [`(movq (,_ ,src) (,_ ,dst)) 
+        (define new-edges (remove dst (remove src live-list)))
+        (graph-add-multiple-edges interference-graph dst new-edges)]
+
+      ; If instr is of the form (callq label), then add an edge (r, v) for every caller-saved register r and every
+      ; variable v in L_after(k) [the interference graph also has registers themselves???? I thought registers were just
+      ; the colors for this graph...]
       [`(callq ,label) (error "Unimplemented for callq")]
+
+      ; Don't do anything for jump?
       [`(jmp ,label) (error "Unimplemented for jmp")]
       [_ (error "Interference-from-live: Unrecognized instruction ~s" instr)])))
 
--- testing/make-interference-tests.rkt
+++ testing/make-interference-tests.rkt
@@ -5,4 +5,15 @@
 (require "test-helpers.rkt") ; for check-fail and check-fail-with-name
 (require "../src/make-interference.rkt")
 
-(define given1)
+(define instrs1 '((movq (int 1) (var v))
+                  (movq (int 46) (var w))
+                  (movq (var v) (var x))
+                  (addq (int 7) (var x))))
+
+(define live-list1 '((v) (v w) (w x) (w x)))
+
+(define expect1 '((x . (w))       ; x interferes with w on instrs 3 and 4
+                  (w . (x v))     ; w interferes with v on instr 2
+                  (v . (w))))
+
+(check-equal? (interference-from-live live-list1 instrs1) expect1)
