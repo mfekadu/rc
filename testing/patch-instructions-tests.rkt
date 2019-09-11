912437660d2a340dd9b41240f0a525f97ee86a96
John Vincent Tan <john.v.tan@gmail.com>
2019-09-11 16:05:33 -0400

    patch instructions DoOne!

--- testing/patch-instructions-tests.rkt
+++ testing/patch-instructions-tests.rkt
@@ -46,6 +46,64 @@
                  (movq (reg rax) (deref rbp -16))))
 (check-equal? (replace-invalid-instrs given3) expect3)
 
+(define given3prog `(program () ((block () (label start) ,given3))))
+(define expect3prog `(program () ((block () (label start) ,@expect3))))
+(check-equal? (patch-instructions given3prog) expect3prog)
 
+(define given4-prog `(program () 
+                        ((block 
+                          ()
+                          (label block176) (movq (int 0) (reg rax)) (jmp conclusion))
+                        (block
+                          ()
+                          (label block178) (addq (deref rbp -8) (deref rbp -16)) (jmp block176))
+                        (block
+                          ()
+                          (label block177) 
+                          (cmpq (deref rbp -8) (deref rbp -16))
+                          (set e (byte-reg al))
+                          (movzbq (byte-reg al) (deref rbp -16)) ; needs to be changed!
+                          (jmp block176))
+                        (block
+                          ()
+                          (label start)
+                          (movq (int 1) (reg rbx))
+                          (movq (int 2) (reg rcx))
+                          (movq (int 3) (reg rdx))
+                          (cmpq (int 1) (int 1))
+                          (jmp-if e block177)
+                          (jmp block178)))))
 
+(define given4-expect `(program () 
+                        ((block 
+                          ()
+                          (label block176) (movq (int 0) (reg rax)) (jmp conclusion))
+                        (block
+                          ()
+                          (label block178)
+                          (movq (deref rbp -8) (reg rax))
+                          (addq (deref rbp -16) (reg rax))
+                          (movq (reg rax) (deref rbp -16))
+                          (jmp block176))
+                        (block
+                          ()
+                          (label block177) 
+                          (movq (deref rbp -8) (reg rax))
+                          (cmpq (deref rbp -16) (reg rax))
+                          (set e (byte-reg al))
+                          (movzbq (byte-reg al) (reg rax))
+                          (movq (reg rax) (deref rbp -16))
+                          (jmp block176))
+                        (block
+                          ()
+                          (label start)
+                          (movq (int 1) (reg rbx))
+                          (movq (int 2) (reg rcx))
+                          (movq (int 3) (reg rdx))
+                          (movq (int 1) (reg rax))
+                          (cmpq (int 1) (reg rax))
+                          (jmp-if e block177)
+                          (jmp block178)))))
+
+(check-equal? (patch-instructions given4-prog) given4-expect)
 (displayln "patch instructions tests pass")
